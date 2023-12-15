2b3e25bee6f03d80465d42a74f4559c211a8fbc6
yeonjin1357 <yeonjin1357@naver.com>
2023-12-15 17:55:39 +0900

    23/12/15 리액트 공부

--- useEffect/src/App.js
+++ useEffect/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -18,13 +18,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </>
   );
 }
 
