7ec0b2151a2dc15521d4054318beed1a03668132
yeonjin1357 <yeonjin1357@naver.com>
2023-12-20 17:58:57 +0900

    23/12/20 리액트 공부

--- practice_03/src/components/Meals/AvailableMeals.js
+++ practice_03/src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -30,11 +32,13 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id}></MealItem>);
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
---
+++ practice_03/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = (props) => {};
+
+export default MealItemForm;
