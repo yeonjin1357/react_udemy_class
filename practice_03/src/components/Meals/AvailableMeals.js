f2d300b700f60d5b4d951552217c22086d499164
yeonjin1357 <yeonjin1357@naver.com>
2023-12-22 17:34:48 +0900

    23/12/22 리액트 공부

--- practice_03/src/components/Meals/AvailableMeals.js
+++ practice_03/src/components/Meals/AvailableMeals.js
@@ -32,7 +32,7 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id}></MealItem>);
+  const mealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}></MealItem>);
 
   return (
     <section className={classes.meals}>
