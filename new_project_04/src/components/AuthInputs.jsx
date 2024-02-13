38ab73ee287b6baefde2f47c8c5909ad1d970c10
yeonjin1357 <yeonjin1357@naver.com>
2024-02-13 21:17:45 +0900

    24/02/13 리액트 강의

---
+++ new_project_04/src/components/AuthInputs.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${({ invalid }) => (invalid ? "#f87171" : "#6b7280")};
+
+  &:hover {
+    color: #f80;
+  }
+`;
+
+export default function AuthInputs() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleInputChange(identifier, value) {
+    if (identifier === "email") {
+      setEnteredEmail(value);
+    } else {
+      setEnteredPassword(value);
+    }
+  }
+
+  function handleLogin() {
+    setSubmitted(true);
+  }
+
+  const emailNotValid = submitted && !enteredEmail.includes("@");
+  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+
+  return (
+    <div id="auth-inputs">
+      <div className="controls">
+        <p>
+          <Label invalid={emailNotValid}>Email</Label>
+          <input type="email" className={emailNotValid ? "invalid" : undefined} onChange={(event) => handleInputChange("email", event.target.value)} />
+        </p>
+        <p>
+          <Label invalid={passwordNotValid}>Password</Label>
+          <input type="password" className={passwordNotValid ? "invalid" : undefined} onChange={(event) => handleInputChange("password", event.target.value)} />
+        </p>
+      </div>
+      <div className="actions">
+        <button type="button" className="text-button">
+          Create a new account
+        </button>
+        <button className="button" onClick={handleLogin}>
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+}
