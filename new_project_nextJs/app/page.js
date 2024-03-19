4b73eedefabfbb55b7dc4a8c017a5396d25543f7
yeonjin1357 <yeonjin1357@naver.com>
2024-03-19 17:20:38 +0900

    next.js 강의

---
+++ new_project_nextJs/app/page.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+import Header from "@/components/header";
+
+export default function Home() {
+  return (
+    <main>
+      <Header></Header>
+      <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href="/about">About Us</Link>
+      </p>
+    </main>
+  );
+}
